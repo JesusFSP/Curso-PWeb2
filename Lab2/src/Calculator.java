2158e17217fe78824505b2e92eef8379127f176b
JesusFSP <jsilva@unsa.edu.pe>
2025-04-22 23:05:56 -0500

    Creando el proyecto Calculator con los metodos que se van a implementar

---
+++ Lab2/src/Calculator.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+
+	}
+
+	public int add(int a, int b){ 
+		return 0; 
+	}
+	public int sub(int a, int b){ 
+		return 0; 
+	}
+	public int mul(int a, int b){ 
+		return 0; 
+	}
+	public int div(int a, int b){ 
+		return 0; 
+	}
+	public int mod(int a, int b){ 
+		return 0; 
+	}
+
+}
