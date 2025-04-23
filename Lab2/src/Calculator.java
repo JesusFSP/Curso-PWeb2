6f36b9ad97dc7e3c576fdc4c8a2574b06c898d88
JesusFSP <jsilva@unsa.edu.pe>
2025-04-22 23:26:15 -0500

    implementando los metodos de la clase calculator

--- Lab2/src/Calculator.java
+++ Lab2/src/Calculator.java
@@ -3,26 +3,36 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese el primer numero: ");
+		int num1 = sc.nextInt(); 
+		System.out.print("Ingrese el segundo numero: ");
+		int num2 = sc.nextInt();
+		
+		System.out.print("La suma es:" + add(num1, num2));
+		System.out.print("\nLa resta es:" + sub(num1, num2));
+		System.out.print("\nLa multiplicacion es:" + mul(num1, num2));
+		System.out.print("\nLa division es:" + div(num1, num2));
+		System.out.print("\nEl modulo es:" + mod(num1, num2));
 
 	}
 
-	public int add(int a, int b){ 
-		return 0; 
+	public static  int add(int a, int b){ 
+		return a+b; 
 	}
-	public int sub(int a, int b){ 
-		return 0; 
+	public static int sub(int a, int b){ 
+		return a-b; 
 	}
-	public int mul(int a, int b){ 
-		return 0; 
+	public static int mul(int a, int b){ 
+		return a*b; 
 	}
-	public int div(int a, int b){ 
-		return 0; 
+	public static int div(int a, int b){ 
+		return a/b; 
 	}
-	public int mod(int a, int b){ 
-		return 0; 
+	public static int mod(int a, int b){ 
+		return a%b; 
 	}
 
 }
