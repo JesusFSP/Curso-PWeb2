4ae9456ced74b4176b8157599b4fa3fc482188a3
JesusFSP <jsilva@unsa.edu.pe>
2025-04-27 00:59:00 -0500

    Implementando la solucion del Ejercicio 03

--- Lab03/Ejercicio03/Ejercicio03.js
+++ Lab03/Ejercicio03/Ejercicio03.js
@@ -1,7 +1,18 @@
-const hoy = new Date();
-const añoActual = hoy.getFullYear();
-console.log(hoy.getMilliseconds());
-const diaArequipa = new Date(añoActual, 3, 28);
-const resta = diaArequipa - hoy
-console.log(resta);
-console.log(Math.ceil(resta / (1000 * 60 * 60 * 24)));
+document .addEventListener('DOMContentLoaded', function(){
+    const hoy = new Date();
+    const añoActual = hoy.getFullYear();
+    const diaArequipa = new Date(añoActual, 7, 15);
+
+    const diferencia = diaArequipa - hoy;
+    const diasFaltantes = Math.ceil(diferencia / (1000 * 60 * 60 * 24));
+
+    if(hoy > diaArequipa){
+        diaArequipa.setFullYear(añoActual + 1)
+    }
+    
+    if(diasFaltantes == 0){
+        document.querySelector('h1').textContent = 'Hoy es el dia de Arequipa';
+    }
+
+    document.getElementById('dias-faltantes').textContent = diasFaltantes;
+});
