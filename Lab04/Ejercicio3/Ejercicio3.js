277804699201adda8c572c7313a7d70acfd97ff5
JesusFSP <jsilva@unsa.edu.pe>
2025-05-06 00:20:15 -0500

    Creando un arreglo para ordenar las 10 regiones con mayor numero de casos

--- Lab04/Ejercicio3/Ejercicio3.js
+++ Lab04/Ejercicio3/Ejercicio3.js
@@ -11,9 +11,37 @@ function loadData() {
         .then(response => response.json())
         .then(data => {
             covidData = data;
-            drawRegionList();
+            drawTop10Regions();
         })
         .catch(error => {
             alert('Error al cargar los datos. Verifique la consola para más detalles.');
         });
+}
+
+function drawTop10Regions() {
+    let regionesConTotales = [];
+    
+    for (let i = 0; i < covidData.length; i++) {
+        let region = covidData[i];
+        let suma = 0;
+        
+        for (let j = 0; j < region.confirmed.length; j++) {
+            suma += parseInt(region.confirmed[j].value);
+        }
+        
+        regionesConTotales.push({
+            nombre: region.region,
+            total: suma
+        });
+    }
+    
+    regionesConTotales.sort(function(a, b) {
+        return b.total - a.total;
+    });
+    
+    let top10Regiones = regionesConTotales.slice(0, 10);
+
+    
+    
+    
 }
