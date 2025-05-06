0f612f520bc33f8be0491820cf8dc32a54cb0fb8
JesusFSP <jsilva@unsa.edu.pe>
2025-05-06 17:44:02 -0500

    Creando la funcion drawArequipaChart con un metodo para encontrar los datos de la region Arequipa

--- Lab04/Ejercicio4/Ejercicio4.js
+++ Lab04/Ejercicio4/Ejercicio4.js
@@ -11,9 +11,27 @@ function loadData() {
         .then(response => response.json())
         .then(data => {
             covidData = data;
-            drawRegionList();
+            drawArequipaChart();
         })
         .catch(error => {
             alert('Error al cargar los datos. Verifique la consola para más detalles.');
         });
+}
+
+function drawArequipaChart() {
+    let arequipaData = null;
+    for (let i = 0; i < covidData.length; i++){
+        if(covidData[i].region === 'Arequipa'){
+            arequipaData = covidData[i];
+            break;
+        }
+    }
+
+    if (!arequipaData) {
+        console.log('No se encontro la region Arequipa')
+    }
+
+    const dataTable = new google.visualizacion.dataTable();
+    dataTable.addColumn('date', 'Fecha');
+    dataTable.addColumn('number', 'Casos Confirmados');
 }
