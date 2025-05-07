44f68eaf79c9291fdea08ec5c9a222ad29d0b1b8
JesusFSP <jsilva@unsa.edu.pe>
2025-05-06 22:06:39 -0500

    Creando la funcion de crecimientoDiario donde excluimos las regiones de lima y callao

--- Lab04/Ejercicio8/Ejercicio8.js
+++ Lab04/Ejercicio8/Ejercicio8.js
@@ -11,9 +11,28 @@ function loadData() {
         .then(response => response.json())
         .then(data => {
             covidData = data;
-            drawRegionList();
+            drawCrecimientoDiarioChart() ;
         })
         .catch(error => {
-            alert('Error al cargar los datos. Verifique la consola para más detalles.');
+            alert('Error al cargar los datos.');
         });
+}
+
+function drawCrecimientoDiarioChart() {
+    let regionesSeleccionadas = [];
+    let regionesEncontradas = 0;
+
+    for (let i = 0; i < covidData.length; i++) {
+        const region = covidData[i];
+        if (region.region !== 'Lima' && region.region !== 'Callao') {
+            regionesSeleccionadas.push(region);
+            regionesEncontradas++;
+            if (regionesEncontradas >= 5) break;
+        }
+    }
+
+    
+
+    const grafico = new google.visualization.ColumnChart(document.getElementById('crecimientoDiarioChart'));
+    grafico.draw(tablaDatos, opciones);
 }
