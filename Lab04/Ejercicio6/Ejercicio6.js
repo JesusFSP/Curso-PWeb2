f7aee62c95cfa8f03981036db90c297f79f7120f
JesusFSP <jsilva@unsa.edu.pe>
2025-05-06 21:12:11 -0500

    Modificando el HTML para representar los elementos que se necesitan, y se creo la funcion drawCrecimientoChart donde se filtro las regiones

--- Lab04/Ejercicio6/Ejercicio6.js
+++ Lab04/Ejercicio6/Ejercicio6.js
@@ -11,9 +11,27 @@ function loadData() {
         .then(response => response.json())
         .then(data => {
             covidData = data;
-            drawRegionList();
+            drawCrecimientoChart();
         })
         .catch(error => {
-            alert('Error al cargar los datos. Verifique la consola para más detalles.');
+            alert('Error al cargar los datos.');
         });
+}
+
+function drawCrecimientoChart() {
+    let regionesFiltradas = [];
+    let contador = 0;
+    
+    for (let i = 0; i < covidData.length; i++) {
+        const region = covidData[i];
+        if (region.region !== 'Lima' && region.region !== 'Callao') {
+            regionesFiltradas.push(region);
+            contador++;
+        }
+    }
+    
+   
+    
+    const chart = new google.visualization.LineChart(document.getElementById('crecimientoChart'));
+    chart.draw(dataTable, options);
 }
