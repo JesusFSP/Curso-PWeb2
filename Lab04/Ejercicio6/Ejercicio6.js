c58f4fae8e13fc40590e4ff963b482d40f24980e
JesusFSP <jsilva@unsa.edu.pe>
2025-05-06 21:19:28 -0500

    Creando la dataTable para agregar los datos al grafico

--- Lab04/Ejercicio6/Ejercicio6.js
+++ Lab04/Ejercicio6/Ejercicio6.js
@@ -20,18 +20,42 @@ function loadData() {
 
 function drawCrecimientoChart() {
     let regionesFiltradas = [];
-    let contador = 0;
-    
+
     for (let i = 0; i < covidData.length; i++) {
         const region = covidData[i];
         if (region.region !== 'Lima' && region.region !== 'Callao') {
             regionesFiltradas.push(region);
-            contador++;
         }
     }
-    
-   
-    
+
+    const dataTable = new google.visualization.DataTable();
+    dataTable.addColumn('date', 'Fecha');
+
+    for (let i = 0; i < regionesFiltradas.length; i++) {
+        dataTable.addColumn('number', regionesFiltradas[i].region);
+    }
+
+    const regionReferencia = regionesFiltradas[0];
+
+    for (let dia = 0; dia < regionReferencia.confirmed.length; dia++) {
+        const partesFecha = regionReferencia.confirmed[dia].date.split('-');
+        const fecha = new Date(`${partesFecha[1]}/${partesFecha[0]}/${partesFecha[2]}`);
+
+        const filaDatos = [fecha];
+
+        for (let r = 0; r < regionesFiltradas.length; r++) {
+            const region = regionesFiltradas[r];
+
+            if (dia < region.confirmed.length) {
+                filaDatos.push(parseInt(region.confirmed[dia].value));
+            } else {
+                filaDatos.push(null);
+            }
+        }
+
+        dataTable.addRow(filaDatos);
+    }
+
     const chart = new google.visualization.LineChart(document.getElementById('crecimientoChart'));
     chart.draw(dataTable, options);
 }
