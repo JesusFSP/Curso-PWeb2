c7de05a38f654cd20fe60759cd5236665c17ea9b
JesusFSP <jsilva@unsa.edu.pe>
2025-06-28 00:20:09 -0500

    Se creo la vista que manejara el formulario

--- git de Django 4 y 5/Django4/personas/views.py
+++ git de Django 4 y 5/Django4/personas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import RawPersonaForm
 
+def personasAnotherCreateView(request):
+    form = RawPersonaForm()
+    context = {
+        'form': form
+    }
+    return render(request, "personas/personasCreate.html", context)
 # Create your views here.
