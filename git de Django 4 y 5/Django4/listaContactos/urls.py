3e27b16fd607feb89e4869c2fd3f7ba6a8911325
JesusFSP <jsilva@unsa.edu.pe>
2025-06-28 01:06:03 -0500

    Se añadio la ruta de la nueva vista y se prueba que se visualice el formulario

--- git de Django 4 y 5/Django4/listaContactos/urls.py
+++ git de Django 4 y 5/Django4/listaContactos/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from personas.views import personasAnotherCreateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('anotherAdd/', personasAnotherCreateView, name='OtroAgregarPersonas'),
 ]
