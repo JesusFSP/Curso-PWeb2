32eddca36b13bb70a734296186640f98311268e8
JesusFSP <jsilva@unsa.edu.pe>
2025-06-28 01:09:16 -0500

    Se modifica la vista para manejar datos POST, validarlos y guardarlos

--- git de Django 4 y 5/Django4/personas/views.py
+++ git de Django 4 y 5/Django4/personas/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from .forms import RawPersonaForm
+from .models import Persona 
 
+def personasAnotherCreateView(request):
+    
+    if request.method == "POST":
+        
+        form = RawPersonaForm(request.POST)
+        if form.is_valid(): 
+            print(form.cleaned_data)
+            
+            Persona.objects.create(**form.cleaned_data)
+            form = RawPersonaForm() 
+        else:
+            print(form.errors) # [cite: 12]
+    else:
+        form = RawPersonaForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "personas/personasCreate.html", context)
 def personasAnotherCreateView(request):
     form = RawPersonaForm()
     context = {
         'form': form
     }
     return render(request, "personas/personasCreate.html", context)
-# Create your views here.
+
