7f0246eccbd2020b0577f2561d0c7bbced56674c
JesusFSP <jsilva@unsa.edu.pe>
2025-06-28 22:52:09 -0500

    Modificando los archivos app.css, app.html y app.ts para poder agregar hobbies

--- Lab09-Angular/proyecto-angular/src/app/app.ts
+++ Lab09-Angular/proyecto-angular/src/app/app.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class App {
-  protected title = 'proyecto-angular';
+export class AppComponent {
+  title = 'my-dream-app';
+  name: string;
+  email: string;
+  webpage: string;
+  hobbies: string[];
+  showHobbies: boolean = false;
+  users: string[] = ['ryan', 'joe', 'cameron', 'john'];
+
+  constructor() {
+    console.log("Constructor working...");
+    this.name = "Carlo Jose Luis";
+    this.email = "ccorrales@unsa.edu.pe";
+    this.webpage = "http://www.unsa.edu.pe";
+    this.hobbies = ["Futbol", "Programacion", "Netflix"];
+  }
+
+  toggleHobbies() {
+    this.showHobbies = !this.showHobbies;
+  }
+
+  newHobby(hobby: HTMLInputElement) {
+    if (hobby.value.trim() !== '') {
+      this.hobbies.push(hobby.value.trim());
+      hobby.value = "";
+    }
+    return false;
+  }
+
+  deleteHobby(index: number) {
+    this.hobbies.splice(index, 1);
+  }
+
+  addUser(newUser: HTMLInputElement) {
+    if (newUser.value.trim() !== '') {
+      this.users.push(newUser.value.trim());
+      newUser.value = '';
+      newUser.focus();
+    }
+    return false;
+  }
+
+  deleteUser(index: number) {
+    this.users.splice(index, 1);
+  }
 }
