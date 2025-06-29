ef998f1e4c305fd2f2182c306246729016bc9d8e
JesusFSP <jsilva@unsa.edu.pe>
2025-06-28 20:34:23 -0500

    Se implemento un archivo estatico que es el style.css para mejorar la vista de todo el proyecto

--- git de Django 4 y 5/Django4/listaContactos/settings.py
+++ git de Django 4 y 5/Django4/listaContactos/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # opcional, carpeta global
+]
 
 
 # Quick-start development settings - unsuitable for production
