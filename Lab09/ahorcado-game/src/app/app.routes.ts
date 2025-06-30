b5e04168adf48d054f1459becca0d81f5edaaacd
JesusFSP <jsilva@unsa.edu.pe>
2025-06-29 21:44:20 -0500

    Configuracion de las rutas

--- Lab09/ahorcado-game/src/app/app.routes.ts
+++ Lab09/ahorcado-game/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { GameBoardComponent } from './game-board/game-board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GameBoardComponent }
+];
