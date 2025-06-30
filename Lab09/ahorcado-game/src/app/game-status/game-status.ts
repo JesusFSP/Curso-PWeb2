e96033ad5d24b872086e3acb002140284c74558b
JesusFSP <jsilva@unsa.edu.pe>
2025-06-29 21:29:10 -0500

    Creando el proyecto del juego del ahoracado con los componentes necesarios

---
+++ Lab09/ahorcado-game/src/app/game-status/game-status.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-status',
+  imports: [],
+  templateUrl: './game-status.html',
+  styleUrl: './game-status.css'
+})
+export class GameStatus {
+
+}
---
+++ Lab09/ahorcado-game/src/app/hangman-display/hangman-display.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hangman-display',
+  imports: [],
+  templateUrl: './hangman-display.html',
+  styleUrl: './hangman-display.css'
+})
+export class HangmanDisplay {
+
+}
