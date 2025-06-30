04418871ac50484400d212fca6c5ddb70c988239
JesusFSP <jsilva@unsa.edu.pe>
2025-06-29 21:42:15 -0500

    Implemtando el estado del juego, si gano o perdio game-status.ts

--- Lab09/ahorcado-game/src/app/game-status/game-status.ts
+++ Lab09/ahorcado-game/src/app/game-status/game-status.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+// game-status.component.ts
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game-status',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './game-status.html',
-  styleUrl: './game-status.css'
+  styleUrls: ['./game-status.css']
 })
-export class GameStatus {
-
+export class GameStatusComponent {
+  @Input() isWon: boolean = false;
+  @Input() isLost: boolean = false;
+  @Input() word: string = '';
+  @Output() newGame = new EventEmitter<void>();
 }
