f9069519ea7d5d6e0886807571392f845436f60d
JesusFSP <jsilva@unsa.edu.pe>
2025-06-29 20:22:31 -0500

    Se volvio a borrar el proyecto y se creo nuevamente por errores, se modifico el archivo app.ts para agregar los hobbies

---
+++ Lab09/my-dream-app/src/app/app.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'my-dream-app';
+  name: string;
+  email: string;
+  webpage: string;
+  hobbies: string[];
+  users: string[];
+
+  constructor() {
+    console.log("Constructor working...");
+    this.name = "Tu Nombre";
+    this.email = "tu@email.com";
+    this.webpage = "http://www.tusitio.com";
+    this.hobbies = ["Fútbol", "Programación", "Netflix"];
+    this.users = ['ryan', 'joe', 'cameron', 'john'];
+  }
+
+  showhobbies() {
+    return true;
+  }
+
+  newHobby(hobby: any) {
+    this.hobbies.push(hobby.value);
+    hobby.value = "";
+    return false;
+  }
+
+  deleteUser(user: string) {
+    this.users = this.users.filter(u => u !== user);
+  }
+
+  addUser(newUser: any) {
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+}
