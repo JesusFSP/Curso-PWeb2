2f7dd2b4db0ae5ece6d64bbe4e77e900cc64aac1
JesusFSP <jsilva@unsa.edu.pe>
2025-07-06 22:45:25 -0500

    Proyecto base listo para deploy en Railway

--- Avance de Proyecto-Final/sazon_peruana/sazon_peruana/settings.py
+++ Avance de Proyecto-Final/sazon_peruana/sazon_peruana/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y5s6ajyt32k1rh=(zty$n8b^8u8k!c20%j!yngg)=xdukp&48%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
